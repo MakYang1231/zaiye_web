59da9548f48a0f4915faf477f55efacbbd49a61d
Mak <makyang.test@gmai.com>
2024-04-26 15:40:47 +0800

    Updates

---
+++ static/7GGXf4yu.js
@@ -0,0 +1,5 @@
+import{_ as o}from"./DHvZbndp.js";import{_ as n}from"./CRx3BvzU.js";import{k as r,H as e,l as _,q as a,o as c}from"./wuzZtZ3Y.js";const s={},i={style:{height:"2000px","margin-top":"5rem","text-align":"center"}};function m(p,f){const t=o;return c(),r("div",i,[e(`
+        ZAIYA-ZINE-PAGE
+        `),_(t,{to:"/"},{default:a(()=>[e(`
+            Home page
+        `)]),_:1})])}const u=n(s,[["render",m]]);export{u as default};
